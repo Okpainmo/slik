291ff12a74be78df6aaaed2cf49357bdec0bc115
Okpainmo <okpainmoandrew@gmail.com>
2022-09-13 03:23:27 +0100

    assets: added project assets

---
+++ assets/data/post-data.js
@@ -0,0 +1,70 @@
+const postsData = [
+  {
+    postIndex: 1,
+    projectId: 'slik',
+    postBanner: 'upload-image',
+    authorName: 'Sam Matthews',
+    slug: 'This-post-title-is-for-Article-1(one)',
+    postTitle: 'This post title is for Article 1(one)',
+    PostSubtitle: 'A follow up to the title of Article 1(one)',
+    postText:
+      'This the post text for the first article. Article 1 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+  },
+  {
+    postIndex: 2,
+    projectId: 'slik',
+    postBanner: 'upload-image',
+    authorName: 'Sarah Stewards',
+    slug: 'This-post-title-is-for-Article-2(two)',
+    postTitle: 'This post title is for Article 2(two)',
+    PostSubtitle: 'A follow up to the title of Article 2(two)',
+    postText:
+      'This the post text for the second article. Article 2 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+  },
+  {
+    postIndex: 3,
+    projectId: 'slik',
+    postBanner: 'upload-image',
+    authorName: 'Karim Ham',
+    slug: 'This-post-title-is-for-Article-3(three)',
+    postTitle: 'This post title is for Article 3(three)',
+    PostSubtitle: 'A follow up to the title of Article 3(three)',
+    postText:
+      'This the post text for the third article. Article 3 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+  },
+  {
+    postIndex: 4,
+    projectId: 'slik',
+    postBanner: 'upload-image',
+    authorName: 'Sunny Ghandi',
+    slug: 'This-post-title-is-for-Article-4(four)',
+    postTitle: 'This post title is for Article 4(four)',
+    PostSubtitle: 'A follow up to the title of Article 4(four)',
+    postText:
+      'This the post text for the forth article. Article 4 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+  },
+  {
+    postIndex: 5,
+    projectId: 'slik',
+    postBanner: 'upload-image',
+    authorName: 'Jackie Lee',
+    slug: 'This-post-title-is-for-Article-5(five)',
+    postTitle: 'This post title is for Article 5(five)',
+    PostSubtitle: 'A follow up to the title of Article 5(five)',
+    postText:
+      'This the post text for the fifth article. Article 5 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+  },
+  {
+    postIndex: 6,
+    projectId: 'slik',
+    postBanner: 'upload-image',
+    authorName: 'Preena Lampur',
+    slug: 'This-post-title-is-for-Article-6(six)',
+    postTitle: 'This post title is for Article 6(six)',
+    PostSubtitle: 'A follow up to the title of Article 6(six)',
+    postText:
+      'This the post text for the sixth article. Article 6 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+  },
+];
+
+export default postsData;
