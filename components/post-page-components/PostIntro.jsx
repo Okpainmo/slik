1c214d6fb836ecbc4dc809d68dead23a6c936481
Okpainmo <okpainmoandrew@gmail.com>
2022-09-21 23:22:35 +0100

    refactor: updates on post page components and footer

--- components/post-page-components/PostIntro.jsx
+++ components/post-page-components/PostIntro.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
 import Image from 'next/image';
-import postBanner from '../../assets/images/1.jpg';
+// import postBanner from '../../assets/images/1.jpg';
 
-function PostIntro() {
+function PostIntro({ currentPost, data }) {
+  // console.log(currentPost);
+
+  const { postTitle, postSubtitle, authorName, postIndex } = currentPost[0];
+
+  const slikMediaData = data.includes.Asset.filter((each) => {
+    return each.fields.title === 'slik-blog-project-asset';
+  });
+
+  const postImage = slikMediaData.filter((each) => {
+    return each.fields.file.fileName.slice(0, 1) == postIndex;
+  });
+
   return (
     <section className='intro-post-details'>
       <div className='flex flex-col p-[12px] xsm:px-[20px] sm:text-center'>
-        <div className='title font-bold text-2xl sm:text-4xl lg:text-5xl montserrat'>
-          Article title
+        <div className='title font-bold text-2xl sm:text-4xl lg:text-5xl montserrat lg:leading-[50px]'>
+          {postTitle}
         </div>
-        <span className='text--colors_secondary title-follow-up sm:pt-4 text-lg xsm:text-xl lg:text-2xl block capitalize'>
-          just a little sub title
+        <span className='text--colors_secondary title-follow-up pt-4 text-lg xsm:text-xl lg:text-2xl block capitalize'>
+          {postSubtitle}
         </span>
       </div>
       <div>
         <div className='lead-post-banner mt-2 sm:mt-4'>
           <Image
-            src={postBanner}
-            alt='post-banner'
-            layout='responsive'
-            objectFit='cover'
-            quality={100}
-            placeholder='blur'
+            src={`https:${postImage[0].fields.file.url}`}
+            width={postImage[0].fields.file.details.image.width}
+            height={1200}
+            priority
           />
         </div>
         <div className='author-section mt-4 sm:mt-6 w-full px-[12px] sm:p-0 mx-auto'>
           <div className='font-bold montserrat mb-1 text-[14px]'>
             <span>Article by </span>
-            <span className='text--colors_secondary'>Author name</span>
+            <span className='text--colors_secondary'>{authorName}</span>
           </div>
         </div>
       </div>
--- components/post-page-components/PostText.jsx
+++ components/post-page-components/PostText.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
 
-function PostText() {
+function PostText({ currentPost }) {
+  const { postText } = currentPost[0];
+
+  const articleParagraphs = postText.content[0].content.map(
+    (each) => each.value
+  );
+
+  // console.log(postText);
   return (
     <section
       className='post-text w-full px-3 xsm:px-[20px] sm:p-0 m-serif mx-auto 
-    text--colors_secondary mt-2 sm:mt-6 sm:text-[18px] text-[16px] leading-[32px]'
+     mt-2 sm:mt-6 sm:text-[18px] text-[16px] leading-[32px]'
     >
-      <p>
-        quod accusantium ipsam nemo sed rerum impedit delectus porro, ea
-        voluptate hic at aliquid illo repellat cumque maxime nam corrupti
-        facilis soluta earum. Nemo, culpa tempor qui ad in veniam quibusdam
-        voluptatibus temporibus magni repellat sequi, ipsum saepe ducimus quod
-        accusantium ipsam nemo sed rerum impedit delectus porro, ea voluptate
-        hic at aliquid illo repellat cumque maxime nam corrupti facilis soluta
-        earum. Nemo, culpa tempora qui ad in veniam quibusdam voluptatibus
-        temporibus magni repellat sequi, ipsum saepe ducimus cupiditate
-        laudantium error ipsam aperiam soluta. Doloremque placeat porro harum
-        illum expedita esse, quaerat recusandae odio officiis nemo fugit quia
-        sit laudantium error animi est similique dolore magni voluptate nesciunt
-        commodi. Iusto quia quam, quaerat laboriosam vitae delectus eius
-        distinctio tempore autem! Tempora veritatis asperiores dignissimos minus
-        culpa aspernatur mollitia voluptatibus illum recusandae labore optio
-        sint nobis ad, ab cum distinctio quisquam quibusdam ipsum magnam
-        voluptatem quidem! Voluptatum iste, quos debitis aliquid dolore id minus
-        fugit iusto ad eum vero non est suscipit ut earum, natus ipsam
-        laboriosam laborum
-      </p>
+      {articleParagraphs.map((paragraph) => {
+        return (
+          <p
+            className='text--colors_secondary pt-6'
+            key={articleParagraphs.indexOf(paragraph)}
+          >
+            {paragraph}
+          </p>
+        );
+      })}
     </section>
   );
 }
