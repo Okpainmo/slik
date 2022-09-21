639e1e476f3ef4c74dde8e8cd7151074f6d9199e
Okpainmo <okpainmoandrew@gmail.com>
2022-09-21 23:18:37 +0100

    next-config: i whitelisted the contentful domain to allow dynamic image import from it

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} **/
+
 module.exports = {
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+
   reactStrictMode: true,
-}
+};
