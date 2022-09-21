283359638474ba0778b0bd3624a52932d94f8a51
Okpainmo <okpainmoandrew@gmail.com>
2022-09-21 23:23:09 +0100

    refactor: updates on main page compoments

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import Hero from '../components/Home-page-components/Hero';
 import PostWrapper from '../components/Home-page-components/PostWrapper';
 import MainAppLayout from '../components/layout-components/MainAppLayout';
-import postsData from '../assets/data/post-data';
+// import postCardsData from '../assets/data/post-data';
 
-function Index() {
+function Index({ data }) {
   return (
     <MainAppLayout>
       <Hero />
-      <PostWrapper postsData={postsData} />
+      <PostWrapper data={data} />
     </MainAppLayout>
   );
 }
 
 export default Index;
+
+export async function getStaticProps() {
+  const url = process.env.SLIK_BLOG_PROJECT_API;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
--- pages/posts/[articleId]/index.jsx
+++ pages/posts/[articleId]/index.jsx
@@ -4,14 +4,28 @@ import PostIntro from '../../../components/post-page-components/PostIntro';
 import PostNav from '../../../components/post-page-components/PostNav';
 import PostText from '../../../components/post-page-components/PostText';
 
-function ArticlePostPage() {
+function ArticlePostPage({ data, params }) {
+  // console.log(data);
+
+  const slikBlogData = data.items.filter((each) => {
+    return each.fields.projectId === 'slik';
+  });
+
+  const postCardsData = slikBlogData.map((each) => {
+    return each.fields;
+  });
+
+  const currentPost = postCardsData.filter(
+    (currentPost) => currentPost.slug === params.articleId
+  );
+
   return (
     <>
       <PostNav />
       <MainAppLayout>
         <div className='w-full sm:w-4/5 lg:w-3/5 mx-auto sm:pt-12'>
-          <PostIntro />
-          <PostText />
+          <PostIntro currentPost={currentPost} data={data} />
+          <PostText currentPost={currentPost} />
         </div>
       </MainAppLayout>
     </>
@@ -19,3 +33,43 @@ function ArticlePostPage() {
 }
 
 export default ArticlePostPage;
+
+export async function getStaticPaths() {
+  const response = await fetch(
+    `https://cdn.contentful.com/spaces/9ejtai3vyo33/environments/master/entries?access_token=x8tHSOxCVVi0YvomIMiiREiyptYCqCe43fneRzNE4lM`
+  );
+  const data = await response.json();
+
+  const slikBlogData = data.items.filter((each) => {
+    return each.fields.projectId === 'slik';
+  });
+
+  const postCardsData = slikBlogData.map((each) => {
+    return each.fields;
+  });
+
+  const paths = postCardsData.map((currentPost) => {
+    return {
+      params: { articleId: `${currentPost.slug}` },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const url = process.env.MY_CONTENTFUL_SPACE_API;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return {
+    props: {
+      data,
+      params,
+    },
+  };
+}
