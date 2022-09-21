761c4e73262c881980be15251556f56612accb2c
Okpainmo <okpainmoandrew@gmail.com>
2022-09-21 23:20:35 +0100

    refactor: updates on landing page components

--- components/Home-page-components/PostCard.jsx
+++ components/Home-page-components/PostCard.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import DemoPic from '../../assets/images/1.jpg';
+// import DemoPic from '../../assets/images/1.jpg';
 
-function PostCard({ dataItems }) {
-  const { postTitle, authorName } = dataItems;
+function PostCard({ postCardsData, data }) {
+  const { postTitle, authorName, slug, postIndex } = postCardsData.fields;
+
+  // const slikMediaData = data.includes.Asset.filter((each) => {
+  //   return each.fields.title === 'slik-blog-project-asset';
+  // });
+
+  const postImage = data.includes.Asset.filter((each) => {
+    return each.fields.file.fileName.slice(0, 1) == postIndex;
+  });
 
   return (
-    <Link href='/posts/articleId'>
+    <Link href={`/posts/${slug}`}>
       <a className='w-full md:w-3/12 md:mx-4'>
         <div className='w-full custom-background mx-auto'>
           <div className='post-image-wrapper w-full relative'>
             <Image
-              alt='post-image'
-              src={DemoPic}
-              layout='responsive'
-              objectFit='cover'
+              src={`https:${postImage[0].fields.file.url}`}
+              width={postImage[0].fields.file.details.image.width}
+              height={1300}
             />
           </div>
           <div className='post-title mt-3 montserrat font-bold text-lg leading-[22px]'>
--- components/Home-page-components/PostWrapper.jsx
+++ components/Home-page-components/PostWrapper.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PostCard from './PostCard';
 
-function PostWrapper({ postsData }) {
-  console.log(postsData);
+function PostWrapper({ data }) {
+  // const slikBlogData = data.items.filter((each) => {
+  //   return each.fields.projectId === 'slik';
+  // });
 
   return (
     <section className='min-h-[200px] mt-4 sm:mt-12 lg:mt-20 lg:mb-14'>
       <div className='mx-3 md:mx-0 md:flex-row md:w-full mt-6 mb-16 flex flex-col justify-center flex-wrap gap-x-[20px] xl:gap-x-[40px] gap-y-[40px]'>
-        {postsData.map(function (each) {
-          return <PostCard key={each.postIndex} dataItems={each} />;
+        {data.items.map(function (each) {
+          return (
+            <PostCard key={each.sys.id} postCardsData={each} data={data} />
+          );
         })}
       </div>
     </section>
