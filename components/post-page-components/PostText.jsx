9777754af655471100b4774c18a652e66dc5649f
Okpainmo <okpainmoandrew@gmail.com>
2022-09-30 12:32:40 +0100

    refactor: code refactoring to aid proper comprehension

--- components/post-page-components/PostText.jsx
+++ components/post-page-components/PostText.jsx
@@ -5,22 +5,24 @@ function PostText({ currentPost }) {
 
   const articleParagraphs = postText.content.map((each) => each);
 
-  // console.log(postText.content);
+  console.log(articleParagraphs);
 
   return (
     <section
       className='post-text w-full px-3 xsm:px-[20px] sm:p-0 m-serif mx-auto 
      mt-2 sm:mt-6 sm:text-[18px] text-[16px] leading-[32px]'
     >
-      {articleParagraphs.map((paragraph) => {
+      {articleParagraphs.map((each) => {
         // console.log(articleParagraphs.indexOf(paragraph));
+
+        const paragraph = each.content[0].value;
 
         return (
           <p
             className='text--colors_secondary pt-6'
-            key={articleParagraphs.indexOf(paragraph)}
+            key={articleParagraphs.indexOf(each)}
           >
-            {paragraph.content[0].value}
+            {paragraph}
           </p>
         );
       })}
