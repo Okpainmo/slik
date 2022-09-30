d61ef94827e21948bf057dc803f382036e24f00c
Okpainmo <okpainmoandrew@gmail.com>
2022-09-30 07:45:17 +0100

    refactor: made updates on data models

--- assets/data/post-data.js
+++ assets/data/post-data.js
@@ -8,7 +8,7 @@ const postsData = [
     postTitle: 'This post title is for Article 1(one)',
     PostSubtitle: 'A follow up to the title of Article 1(one)',
     postText:
-      'This the post text for the first article. Article 1 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+      'Just add any long dummy text',
   },
   {
     postIndex: 2,
@@ -19,7 +19,7 @@ const postsData = [
     postTitle: 'This post title is for Article 2(two)',
     PostSubtitle: 'A follow up to the title of Article 2(two)',
     postText:
-      'This the post text for the second article. Article 2 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+      'Just add any long dummy text',
   },
   {
     postIndex: 3,
@@ -30,7 +30,7 @@ const postsData = [
     postTitle: 'This post title is for Article 3(three)',
     PostSubtitle: 'A follow up to the title of Article 3(three)',
     postText:
-      'This the post text for the third article. Article 3 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+      'Just add any long dummy text',
   },
   {
     postIndex: 4,
@@ -41,7 +41,7 @@ const postsData = [
     postTitle: 'This post title is for Article 4(four)',
     PostSubtitle: 'A follow up to the title of Article 4(four)',
     postText:
-      'This the post text for the forth article. Article 4 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+      'Just add any long dummy text',
   },
   {
     postIndex: 5,
@@ -52,7 +52,7 @@ const postsData = [
     postTitle: 'This post title is for Article 5(five)',
     PostSubtitle: 'A follow up to the title of Article 5(five)',
     postText:
-      'This the post text for the fifth article. Article 5 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+      'Just add any long dummy text',
   },
   {
     postIndex: 6,
@@ -63,7 +63,7 @@ const postsData = [
     postTitle: 'This post title is for Article 6(six)',
     PostSubtitle: 'A follow up to the title of Article 6(six)',
     postText:
-      'This the post text for the sixth article. Article 6 is unique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore nulla temporibus possimus',
+      'Just add any long dummy text',
   },
 ];
 
---
+++ assets/data/post-media-data.js
@@ -0,0 +1,14 @@
+const slikImageData = [
+  {
+    // use this same data for all the images
+
+    Title: 'slik-blog-project-asset',
+    Description: 'post-banner',
+    File: 'add image here',
+
+    /* note that the name of the image should match the postIndex of the post. 
+    If postIndex is 1, then save the image with something like 1.jpg. This is 
+    not a contentful rule, but just another hack to help me query and get the data
+    as I want them */
+  },
+];
