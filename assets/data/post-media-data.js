69661f0b70391749868350f6a87241667a5e773b
Okpainmo <okpainmoandrew@gmail.com>
2022-09-30 11:03:14 +0100

    refactor: made an update on a data model comment

--- assets/data/post-media-data.js
+++ assets/data/post-media-data.js
@@ -6,7 +6,8 @@ const slikImageData = [
     Description: 'post-banner',
     File: 'add image here',
 
-    /* note that the name of the image should match the postIndex of the post. 
+    /* note that the name of each image should match the postIndex as shown in the 
+    post data model. 
     If postIndex is 1, then save the image with something like 1.jpg. This is 
     not a contentful rule, but just another hack to help me query and get the data
     as I want them */
